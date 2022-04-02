b6c6f08e3221eebb23161ff4277e51f708d1ad9d
Ameera jabban <Ameera.a.jabban@hotmail.com>
2022-04-03 01:22:29 +0300

    Add Context

---
+++ RenderDesignWeb/Context/RenderDesignContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RenderDesignWeb.Models;
+
+namespace RenderDesignWeb.Context
+{
+    public class RenderDesignContext : DbContext
+    {
+        public RenderDesignContext(DbContextOptions<RenderDesignContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+      
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=RenderDesign_db;Trusted_Connection=True;");
+        }
+    }
+}
